4ad1553d8f92b923fa2dc27296e724801c5d9832
Celosz-fle7 <celoszfle7@gmail.com>
2026-05-27 07:58:04 +0700

    add tv section model

---
+++ lib/core/models/tv_home_section.dart
@@ -0,0 +1,14 @@
+class TVHomeSection {
+
+  final String id;
+
+  final String title;
+
+  final List<dynamic> items;
+
+  const TVHomeSection({
+    required this.id,
+    required this.title,
+    required this.items,
+  });
+}
