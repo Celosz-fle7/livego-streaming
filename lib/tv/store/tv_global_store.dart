9c35b58ffd3d9deb2951695eeec17e1e2b6daff0
Celosz-fle7 <celoszfle7@gmail.com>
2026-05-27 12:11:53 +0700

    fix global store inspector access

--- lib/tv/store/tv_global_store.dart
+++ lib/tv/store/tv_global_store.dart
@@ -1,41 +1,23 @@
 class TVGlobalStore {
+  static final Map<String, dynamic> store = {};
 
-  static final Map<String, dynamic>
-      _store = {};
-
-  static void set(
-    String key,
-    dynamic value,
-  ) {
-
-    _store[key] = value;
+  static void set(String key, dynamic value) {
+    store[key] = value;
   }
 
-  static dynamic get(
-    String key,
-  ) {
-
-    return _store[key];
+  static dynamic get(String key) {
+    return store[key];
   }
 
-  static bool has(
-    String key,
-  ) {
-
-    return _store.containsKey(
-      key,
-    );
+  static bool has(String key) {
+    return store.containsKey(key);
   }
 
-  static void remove(
-    String key,
-  ) {
-
-    _store.remove(key);
+  static void remove(String key) {
+    store.remove(key);
   }
 
   static void clear() {
-
-    _store.clear();
+    store.clear();
   }
 }
--- lib/tv/store/tv_state_inspector.dart
+++ lib/tv/store/tv_state_inspector.dart
@@ -1,12 +1,7 @@
 import 'tv_global_store.dart';
 
 class TVStateInspector {
-
   static int totalKeys() {
-
-    return TVGlobalStore
-        ._store
-        .keys
-        .length;
+    return TVGlobalStore.store.keys.length;
   }
 }
